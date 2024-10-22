b548b8293cacec49ae6ddcc9c5f858ac227fb4f8
Z-WICK <71963565+Z-WICK@users.noreply.github.com>
2024-10-22 17:02:21 +0800

    feat: 全局状态管理

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,31 +1,42 @@
 <template>
-  <div id="globalHeader">
-    <a-menu
-      mode="horizontal"
-      :selected-keys="selectedKeys"
-      @menu-item-click="doMenuClick"
-    >
-      <a-menu-item
-        key="0"
-        :style="{ padding: 0, marginRight: '38px' }"
-        disabled
+  <a-row
+    id="globalHeader"
+    class="grid-demo"
+    style="margin-bottom: 16px"
+    align="center"
+  >
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        @menu-item-click="doMenuClick"
       >
-        <div class="title-bar">
-          <img class="logo" src="../assets/ic_contact.jpg" />
-          <div class="title">SionOj</div>
-        </div>
-      </a-menu-item>
-      <a-menu-item v-for="item in routes" :key="item.path"
-        >{{ item.name }}
-      </a-menu-item>
-    </a-menu>
-  </div>
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/ic_contact.jpg" />
+            <div class="title">SionOj</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path"
+          >{{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>{{ store.state.user?.loginUser?.userName ?? "未登录" }}</div>
+    </a-col>
+  </a-row>
 </template>
 
 <script setup lang="ts">
 import { routes } from "@/router/routes";
 import { useRouter } from "vue-router";
 import { ref } from "vue";
+import { useStore } from "vuex";
 
 const router = useRouter();
 
@@ -41,6 +52,14 @@ const doMenuClick = (key: string) => {
     path: key,
   });
 };
+
+const store = useStore();
+
+setTimeout(() => {
+  store.dispatch("user/getLoginUser", {
+    userName: "Sion",
+  });
+}, 3000);
 </script>
 <style scoped>
 .title-bar {
---
+++ src/store/user.ts
@@ -0,0 +1,30 @@
+import { StoreOptions } from "vuex";
+
+export default {
+  // 启用命名空间，防止模块间的命名冲突
+  namespaced: true,
+  // 定义模块的初始状态，这里包含登录用户的信息
+  state: () => ({
+    loginUser: {
+      userName: "未登录",
+    },
+  }),
+  // 定义模块的 actions，用于处理异步操作或复杂逻辑
+  actions: {
+    // 获取登录用户信息的 action
+    getLoginUser({ commit, state }, payload) {
+      // todo 改为从远程请求获取登录信息
+      // 这里通过 commit 提交一个假数据 'sion'，模拟更新用户状态
+      commit("updateUser", { userName: "sion" });
+    },
+  },
+  // 定义 mutations，用于直接修改 state
+  mutations: {
+    // 更新用户信息的 mutation
+    updateUser(state, payload) {
+      // 更新 loginUser 的值为传入的 payload
+      state.loginUser = payload;
+    },
+  },
+  // 使用 TypeScript 的类型断言，声明这是一个 Vuex 的模块配置
+} as StoreOptions<any>;
