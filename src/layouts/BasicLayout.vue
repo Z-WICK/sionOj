314d5a5c78c0e13a589ebaed59a17d14df7b40d7
Z-WICK <71963565+Z-WICK@users.noreply.github.com>
2024-10-22 15:56:39 +0800

    feat: 通用布局开发

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        Sion ©2024 Created by Ant Design
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #f5f5f5, #fff);
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
