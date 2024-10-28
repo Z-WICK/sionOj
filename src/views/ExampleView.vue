e74cb1ccfad2bfe00e3c3328159f6398ac8d3181
Z-WICK <71963565+Z-WICK@users.noreply.github.com>
2024-10-28 12:24:38 +0800

    feat: 整合 Monaco Editor 代码编辑器

---
+++ src/views/ExampleView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="exampleView">
+    <CodeEditor :value="codeValue" :handle-change="OnCodeChange" />
+    <MdEditor :value="mdValue" :handle-change="onMdChange" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import MdEditor from "@/components/MdEditor.vue";
+import CodeEditor from "@/components/CodeEditor.vue"; // @ is an alias to /src
+
+const codeValue = ref();
+const mdValue = ref();
+
+const OnCodeChange = (v: string) => {
+  codeValue.value = v;
+};
+
+const onMdChange = (v: string) => {
+  mdValue.value = v;
+};
+</script>
