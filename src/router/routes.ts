22007882c3f48da72fcb23aa1471ab2820bfef2f
Z-WICK <71963565+Z-WICK@users.noreply.github.com>
2024-10-29 15:35:18 +0800

    feat: 管理题目页面开发

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,6 +7,7 @@ import UserLoginView from "@/views/user/UserLoginView.vue";
 import UserRegisterView from "@/views/user/UserRegisterView.vue";
 import ACCESS_ENUM from "@/access/accessEnum";
 import AddQuestionView from "@/views/question/AddQuestionView.vue";
+import ManageQuestionView from "@/views/question/ManageQuestionView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
@@ -32,12 +33,21 @@ export const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
-    path: "/add",
+    path: "/add/question",
     name: "创建题目",
     component: AddQuestionView,
-    meta: {
-      access: ACCESS_ENUM.ADMIN,
-    },
+    // meta: {
+    //   access: ACCESS_ENUM.ADMIN,
+    // },
+  },
+
+  {
+    path: "/manage/question",
+    name: "管理题目",
+    component: ManageQuestionView,
+    // meta: {
+    //   access: ACCESS_ENUM.ADMIN,
+    // },
   },
   {
     path: "/hide",
